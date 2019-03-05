5eb2bda57c17188a6c5b0317b04a25e6cdaadad7
Oliver Grant <olivergrant35@gmail.com>
2019-03-05 13:27:21 +0000

    QR code scanning working

--- app/src/main/java/com/olivergrant/oliver/easytimesheet/Homepage.java
+++ app/src/main/java/com/olivergrant/oliver/easytimesheet/Homepage.java
@@ -1,32 +1,45 @@
 package com.olivergrant.oliver.easytimesheet;
 
-import android.content.Context;
+import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.hardware.Camera;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.util.SparseArray;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.view.View;
 import android.widget.Button;
-import android.widget.FrameLayout;
-import android.widget.ImageButton;
+import android.widget.TextView;
+
+import com.google.android.gms.vision.CameraSource;
+import com.google.android.gms.vision.Detector;
+import com.google.android.gms.vision.barcode.Barcode;
+import com.google.android.gms.vision.barcode.BarcodeDetector;
+
+import java.io.IOException;
 
 @SuppressWarnings("deprecation")
 public class Homepage extends AppCompatActivity {
 
-    Camera cam;
-    FrameLayout frameLayout;
+
+    SurfaceView surfaceView;
+    CameraSource cameraSource;
+    BarcodeDetector barcodeDetector;
+
+    TextView textViewStatus;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homepage);
-        frameLayout = (FrameLayout)findViewById(R.id.frameLayoutCamera);
-        //TODO: Get Camera to open and display correctly in the box.
-        //TODO: Camera get instance as i enabled it in phone permissions. Need to make it so it requests permission. 
-        cam = getCameraInstance();
+        //TODO: Camera get instance as i enabled it in phone permissions. Need to make it so it requests permission.
+        surfaceView = (SurfaceView) findViewById(R.id.cameraPreview);
+        textViewStatus = (TextView) findViewById(R.id.textViewStatusUpdate);
 
+        startScanning();
 
         //Getting that buttons
         final Button buttonChangeSignInMethod = findViewById(R.id.buttonChangeSignInMethod);
@@ -36,48 +49,60 @@ public class Homepage extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 //Start the sign in with code activity
-                    startActivity(new Intent(Homepage.this, activity_codeScreen.class));
+                startActivity(new Intent(Homepage.this, activity_codeScreen.class));
             }
         });
     }
 
-//    private boolean safeCameraOpen(int id) {
-//        boolean qOpened = false;
-//
-//        try {
-//            releaseCameraAndPreview();
-//            camera = Camera.open(id);
-//            qOpened = (camera != null);
-//        } catch (Exception e) {
-//            Log.e(getString(R.string.app_name), "failed to open Camera");
-//            e.printStackTrace();
-//        }
-//
-//        return qOpened;
-//    }
+    //Method will start and display the camera and also scan for QR codes.
+    private void startScanning(){
+        barcodeDetector = new BarcodeDetector.Builder(this)
+                .setBarcodeFormats(Barcode.QR_CODE).build();
 
-    //Determining which camera is the front facing one and getting it's object.
-    public static Camera getCameraInstance(){
-        Camera c = null;
-        try {
-            c = Camera.open(); // attempt to get a Camera instance
-        }
-        catch (Exception e){
-            Log.e("CAMERA ERROR", e.getMessage());
-        }
-        return c; // returns null if camera is unavailable
-    }
+        //TODO: uncomment setFacing to change camera to front cam. Set to rear for testing.
+        cameraSource = new CameraSource.Builder(this, barcodeDetector)
+                //.setFacing(1)
+                .setRequestedPreviewSize(300, 300).build();
 
-    /*Check to see if the device has a camera. Might not need this method as application would
-        be on hardware given to client so would always have a camera.
-     */
-    private boolean checkCameraHardware(Context context) {
-        if (context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA)){
-            // this device has a camera
-            return true;
-        } else {
-            // no camera on this device
-            return false;
-        }
+        surfaceView.getHolder().addCallback(new SurfaceHolder.Callback() {
+            @Override
+            public void surfaceCreated(SurfaceHolder holder) {
+                if (ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                    return;
+                }
+                try {
+                    cameraSource.start(holder);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
+            }
+
+            @Override
+            public void surfaceDestroyed(SurfaceHolder holder) {
+                cameraSource.stop();
+            }
+        });
+
+        barcodeDetector.setProcessor(new Detector.Processor<Barcode>() {
+            @Override
+            public void release() {
+
+            }
+
+            @Override
+            public void receiveDetections(Detector.Detections<Barcode> detections) {
+                SparseArray<Barcode> qrCodes = detections.getDetectedItems();
+
+                if(qrCodes.size() !=0){
+                    Log.d("QRCodes", qrCodes.valueAt(0).displayValue);
+                    ((TextView)findViewById(R.id.textViewStatusUpdate)).setText(qrCodes.valueAt(0).displayValue);
+                }
+            }
+        });
     }
 }
